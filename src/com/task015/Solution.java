520aead32c445392c67cb764801bd50d35a849de
Serge Kravchenko <leetovetz2d3danimator@gmail.com>
2022-02-24 21:09:44 +0200

    task015
    task015

---
+++ src/com/task015/Solution.java
@@ -0,0 +1,12 @@
+package com.task015;
+
+public class Solution {
+    public static void main(String[] args) {
+        int result = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            result = i * 5;
+            System.out.println(i + " x " + 5 + " = " + result);
+        }
+    }
+}
