11129fe2ee06d927b9df17a1dc2e91b2ddb93241
Serge Kravchenko <leetovetz2d3danimator@gmail.com>
2022-03-25 22:31:56 +0200

    task018
    task018

---
+++ src/com/task018/Solution.java
@@ -0,0 +1,15 @@
+package com.task018;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = new int[]{0, 5, 2, 4, 7, 1, 3, 19};
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 != 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
